f2314d6e8b87d6beafa3c4a48ea925dfe33a2e6b
pdashevski <paveldashevski@gmail.com>
2020-06-25 00:29:01 +0300

    task's_update

--- 2.JavaCore/src/com/javarush/task/task17/task1720/BankAccount.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1720/BankAccount.java
@@ -15,23 +15,23 @@ public class BankAccount {
         this.owner = owner;
     }
 
-    public void deposit(BigDecimal money) {
-        synchronized (balance) {
+    public synchronized void deposit(BigDecimal money) {
+
             BigDecimal newBalance = balance.add(money);
             System.out.println("Добавляем " + money + ", на счету " + newBalance);
             balance = newBalance;
-        }
+
     }
 
-    public void withdraw(BigDecimal money) throws NotEnoughMoneyException {
-        synchronized (balance) {
+    public synchronized void withdraw(BigDecimal money) throws NotEnoughMoneyException {
+
             BigDecimal newBalance = balance.subtract(money);
 
             if (newBalance.compareTo(BigDecimal.ZERO) < 0) throw new NotEnoughMoneyException();
 
             balance = newBalance;
             System.out.println("Тратим " + money + ", на счету " + balance);
-        }
+
     }
 
     public void deposit(String money) {
