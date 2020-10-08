d5638dd50fc9eb47c73baeb8808cc155805d3752
Pavel Dashevski <paveldashevski@gmail.com>
2020-10-08 20:08:35 +0300

    task's_update

--- 2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
@@ -11,6 +11,28 @@ public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        //запоминаем настоящий PrintStream в специальную переменную
+        PrintStream consoleStream = System.out;
+
+        //Создаем динамический массив
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        //создаем адаптер к классу PrintStream
+        PrintStream stream = new PrintStream(outputStream);
+        //Устанавливаем его как текущий System.out
+        System.setOut(stream);
+
+        //Вызываем функцию, которая ничего не знает о наших манипуляциях
+        testString.printSomething();
+
+        //Преобразовываем записанные в наш ByteArray данные в строку
+        String s = outputStream.toString();
+        //Возвращаем все как было
+        System.setOut(consoleStream);
+        StringBuilder stringBuilder = new StringBuilder(s);
+
+        String replaceStr = stringBuilder.toString().replace("te", "??");
+        //выводим ее в консоль
+        System.out.println(replaceStr);
     }
 
     public static class TestString {
