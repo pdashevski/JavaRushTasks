9815032735d9cfcdc8ec65b903678e022cc42eed
Pavel Dashevski <paveldashevski@gmail.com>
2020-10-08 20:16:16 +0300

    task's_update

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1913/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task19.task1913;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Выводим только цифры
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        //запоминаем настоящий PrintStream в специальную переменную
+        PrintStream consoleStream = System.out;
+
+        //Создаем динамический массив
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        //создаем адаптер к классу PrintStream
+        PrintStream stream = new PrintStream(outputStream);
+        //Устанавливаем его как текущий System.out
+        System.setOut(stream);
+
+        //Вызываем функцию, которая ничего не знает о наших манипуляциях
+        testString.printSomething();
+
+        //Преобразовываем записанные в наш ByteArray данные в строку
+        String s = outputStream.toString();
+        //Возвращаем все как было
+        System.setOut(consoleStream);
+        StringBuilder stringBuilder = new StringBuilder(s);
+
+        String onlyNum = stringBuilder.toString().replaceAll("\\D", "");
+        //выводим ее в консоль
+        System.out.println(onlyNum);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
