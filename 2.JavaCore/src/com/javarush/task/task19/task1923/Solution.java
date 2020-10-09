fd00531e54a6b9f19e06f193dd02298a5f3624ae
Pavel Dashevski <paveldashevski@gmail.com>
2020-10-09 23:32:17 +0300

    task's_update

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task19.task1923;
+
+import java.io.*;
+
+/* 
+Слова с цифрами
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(args[0]));
+             FileWriter fileWriter = new FileWriter(args[1])) {
+            String fileReadLine;
+            String[] splitedLine;
+            while ((fileReadLine = fileReader.readLine()) != null) {
+                splitedLine = fileReadLine.split(" ");
+                for (String word : splitedLine) {
+                    if (word.matches(".+[0-9].+")) {
+                        fileWriter.write(word + " ");
+                    }
+                }
+            }
+        }
+
+    }
+}
