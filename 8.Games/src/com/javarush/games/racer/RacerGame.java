82411caed64d40fbde843ba894422d035ad057f3
Pavel Dashevski <paveldashevski@gmail.com>
2020-12-19 01:09:56 +0300

    race game 4 update

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -6,10 +6,44 @@ public class RacerGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
 
+    public static final int CENTER_X = WIDTH / 2;
+    public static final int ROADSIDE_WIDTH = 14;
+
     @Override
     public void initialize() {
         super.initialize();
         setScreenSize(WIDTH, HEIGHT);
+        createGame();
         showGrid(false);
+    }
+
+    private void createGame() {
+        drawScene();
+    }
+
+    private void drawScene() {
+        drawField();
+    }
+
+    private void drawField() {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (x == CENTER_X) {
+                    setCellColor(CENTER_X, y, Color.WHITE);
+                } else if (x >= ROADSIDE_WIDTH && x < (WIDTH - ROADSIDE_WIDTH)) {
+                    setCellColor(x, y, Color.DIMGRAY);
+                } else {
+                    setCellColor(x, y, Color.GREEN);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void setCellColor(int x, int y, Color color) {
+        if (x > WIDTH - 1 || x < 0 || y < 0 || y > HEIGHT - 1) {
+            return;
+        }
+        super.setCellColor(x, y, color);
     }
 }
